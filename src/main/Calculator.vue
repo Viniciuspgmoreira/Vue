faa1fb71cbcdbbe97857fd30c45d0834f98e90ec
Vinicius Moreira <viniciuspgmoreira@hotmail.com>
2021-03-24 10:15:52 -0300

    Added addbutton logic

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -44,7 +44,20 @@ export default {
       console.log("Operação" + operation);
     },
     addDigit(n) {
-      console.log("Digito" + n);
+      if (n === "." && this.displayValue.includes(".")) {
+        return;
+      }
+      const clearDisplay = this.displayValue === "0" || this.clearDisplay;
+      const currentValue = clearDisplay ? "" : this.displayValue;
+      const displayValue = currentValue + n;
+      this.displayValue = displayValue;
+      this.clearDisplay = false;
+
+      if (n !== ".") {
+        const i = this.current;
+        const newValue = parseFloat(displayValue);
+        this.values[i] = newValue;
+      }
     },
   },
 };
